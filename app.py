aa38c7d8e7129650d08e8b8013b74262db0d5dfe
SMART ADMINISTRADOR <pipebenavides9427@gmail.com>
2025-08-03 09:17:20 -0500

    Primer commit: subir proyecto Go Travel

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/gps')
+def gps():
+    return render_template('gps.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@app.route('/lugares')
+def lugares():
+    return render_template('lugares.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
