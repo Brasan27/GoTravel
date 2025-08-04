04aae5236a0cda3484831d704074d2090e1cfd55
Brasan27 <pipebenavides9427@gmail.com>
2025-08-04 15:24:42 -0500

    Versión 1.1

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
-def home():
-    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/gps')
 def gps():
     return render_template('gps.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/registro')
 def registro():
